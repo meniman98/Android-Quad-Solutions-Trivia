f076179b2f9f2df64782be950ed1f069293732e2
Huncho Jack <meniman98@gmail.com>
2021-05-21 17:53:57 +0200

    added Question model

---
+++ app/src/main/java/com/example/quadsolutionstrivia/Result.kt
@@ -0,0 +1,4 @@
+package com.example.quadsolutionstrivia
+
+class Result {
+}
--- app/src/test/java/com/example/quadsolutionstrivia/ExampleUnitTest.kt
+++ app/src/test/java/com/example/quadsolutionstrivia/ExampleUnitTest.kt
@@ -10,8 +10,22 @@ import org.junit.Assert.*
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 class ExampleUnitTest {
+
+    val myQuestion = Question()
     @Test
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
+    }
+
+    @Test
+    fun question() {
+        myQuestion.category = "hi"
+        myQuestion.category = "ello"
+        assertEquals("ello", myQuestion.category)
+    }
+
+    @Test
+    fun isNull() {
+        assertEquals(null, myQuestion.correct_answer)
     }
 }
