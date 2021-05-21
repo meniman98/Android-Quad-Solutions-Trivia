068ab75d2d96330ade7fee02dda04dade960ac7f
Huncho Jack <meniman98@gmail.com>
2021-05-21 17:01:03 +0200

    the very first

---
+++ app/src/main/java/com/example/quadsolutionstrivia/Question.kt
@@ -0,0 +1,11 @@
+package com.example.quadsolutionstrivia
+
+class Question {
+    var category: String? = null
+    var type: String? = null
+    var difficulty: String? = null
+    var question: String? = null
+    var correct_answer: String? = null
+    var incorrect_answers: List<String>? = null
+
+}
